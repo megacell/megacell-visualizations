a0e49d940d1f70ab6e4671d32c2aa34b88070e27
Chenyang Yuan <yuanchenyang@gmail.com>
2015-04-28 23:16:00 -0700

    Changed extraction

--- extract_all.py
+++ extract_all.py
@@ -6,7 +6,7 @@ import extract_routes
 def main():
     conn = psycopg2.connect(database='geodjango', user='megacell')
     try:
-        extract_OD.execute(conn, 'web/data/od_route_polygons.geojson', routes=True)
+        #extract_OD.execute(conn, 'web/data/od_route_polygons.geojson', routes=True)
         #extract_OD.execute(conn, 'data/od_trajectory_polygons.geojson', routes=False)
         extract_routes.execute(conn, 'web/data/routes_paths.geojson')
     finally:
--- extract_routes.py
+++ extract_routes.py
@@ -12,13 +12,23 @@ SELECT ST_AsGeoJSON(geom), flow_count
 FROM experiment2_routes
 WHERE orig_taz=%s;
 '''
-OD_sql = '''
-SELECT ST_AsGeoJSON(ST_ConvexHull(ST_Collect(start_point)))
+
+od_routes_sql = '''
+SELECT ST_AsGeoJSON(geom), flow_count
 FROM experiment2_routes
-WHERE orig_taz=%s;
+WHERE orig_taz=%s and dest_taz=%s;
 '''
 
-ORIGIN_ID = 21107000
+OD_sql = '''
+SELECT ST_AsGeoJSON(ST_ConvexHull(ST_Collect({0}_point)))
+FROM experiment2_routes
+WHERE {1}_taz=%s;
+'''
+orig_sql = OD_sql.format('start','orig')
+dest_sql = OD_sql.format('end','dest')
+
+ORIG_ID = 22113000 # 21107000
+DEST_ID = 22091000
 
 def execute(conn, outfile):
 
@@ -26,12 +36,15 @@ def execute(conn, outfile):
     fc = FeatureCollection()
     cmap = gyr_cmap(100)
 
-    cur.execute(routes_sql, (ORIGIN_ID,))
+    #cur.execute(routes_sql, (ORIG_ID,))
+    cur.execute(od_routes_sql, (ORIG_ID, DEST_ID))
     all_data = cur.fetchall()
     for geom, flow_count in all_data:
         fc.add(json.loads(geom), {'stroke': cmap(flow_count)})
 
-    cur.execute(OD_sql, (ORIGIN_ID,))
+    cur.execute(orig_sql, (ORIG_ID,))
+    fc.add(json.loads(cur.fetchone()[0]), {})
+    cur.execute(dest_sql, (DEST_ID,))
     fc.add(json.loads(cur.fetchone()[0]), {})
 
     fc.dump(open(outfile, 'w'))
